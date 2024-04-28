a61cf903a609fed70021cd01f877afc178923328
micatoff <kotofcat@gmail.com>
2024-04-28 11:14:39 +0300

    Reapply "[Fix]: dynamic client celect"
    This reverts commit 8d1917eff95429c93203af1ac4295545c3c20b3e.

--- src/views/WorkerView.vue
+++ src/views/WorkerView.vue
@@ -4,13 +4,12 @@ import Header from '@components/Header.vue';
 
 import { ref, onMounted, watchEffect, computed } from 'vue'
 import { auth } from '@/helpers'
-import { authStore } from '@/stores/auth'
+import { authStore, useClientStore } from '@/stores/auth'
 import moment from 'moment'
 
 
 let employee_name = authStore.get('employee_name')
-
-let client_cached = authStore.get('client')
+const clientStore = useClientStore()
 
 let date_cached_text = authStore.get('date')
 let company_id_cached = authStore.get('company_id')
@@ -23,18 +22,6 @@ let date_cached =  moment(date_cached_text, 'DD.MM.YYYY')
 let date_default = date_cached.isValid() ? date_cached.toDate() : null
 const date = ref(date_default)
 let clients = ref([])
-
-let null_client = {
-	id: 0,
-	name: '',
-	time_start: ``,
-	time_end: ``,
-	service: '',
-}
-
-let client_default = client_cached ?? null_client
-
-let client_selected = ref(client_default)
 
 function format_date(date) {
 	return moment(date).format('DD.MM.YYYY')
@@ -56,20 +43,17 @@ let wasFirstCompanyChange = false
 watchEffect(async () => {
 	authStore.set('company_id', company_selected.value)
 	if (wasFirstCompanyChange) {
-		client_selected.value = null_client
+		clientStore.setNull()
 	}
 	wasFirstCompanyChange = true
 })
 
 watchEffect(async () => {
+	let date_prev_text = authStore.get('date')
 	authStore.set('date', date_formatted.value)
-	if (date_formatted.value !== date_cached_text) {
-		client_selected.value = null_client
+	if (date_formatted.value !== date_prev_text) {
+		clientStore.setNull()
 	}
-})
-
-watchEffect(async () => {
-	authStore.set('client', client_selected.value)
 })
 
 async function fetch_companies() {
@@ -118,7 +102,7 @@ async function fetch_clients(company_id, date) {
 
 			<div class="overflow-y-auto">
 				<div v-for="item in clients" class="border border-gray-500 rounded-lg p-2 mt-4 cursor-pointer"
-					:class="{ 'bg-gray-200 dark:bg-gray-700': item.id === client_selected.id }" @click="client_selected = item">
+					:class="{ 'bg-gray-200 dark:bg-gray-700': item.id === clientStore.client.id }" @click="clientStore.set(item)">
 					<div class="border-b border-gray-400 mb-1">{{ item.time_start }} - {{ item.time_end }}</div>
 					<div class="mb-2">{{ item.service }}</div>
 					<div>{{ item.name }}</div>
@@ -129,7 +113,7 @@ async function fetch_clients(company_id, date) {
 			<Header :name="employee_name" />
 
 			<div class="px-5 py-2">
-				<div v-if="client_selected.name && clients.length > 0" class="mb-1">Клинет: {{ client_selected.name }}</div>
+				<div v-if="clientStore.client.name && clients.length > 0" class="mb-1">Клинет: {{ clientStore.client.name }}</div>
 				<RouterLink to="/worker/history" active-class="font-bold">
 					<PrimaryBtn class="me-8 mb-5">История посещений</PrimaryBtn>
 				</RouterLink>
@@ -138,7 +122,7 @@ async function fetch_clients(company_id, date) {
 					<PrimaryBtn>Медкарта</PrimaryBtn>
 				</RouterLink>
 
-				<div v-if="client_selected.id && company_selected" class="border rounded-lg p-4 ">
+				<div v-if="clientStore.client.id && company_selected" class="border rounded-lg p-4 ">
 					<RouterView />
 				</div>
 
